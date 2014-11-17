fea899b18433e75888379936daa5a2a72837aa2c
jonschlinkert <dev@sellside.com>
2014-11-17 07:31:03 -0500

    0.1.2

--- utils/basename.js
+++ utils/basename.js
@@ -3,6 +3,10 @@
 var path = require('path');
 var ext = require('./ext');
 
+/**
+ * `basename` of a file, excluding extension
+ */
+
 module.exports = function basename(filepath) {
   return path.basename(filepath, ext(filepath));
 };
